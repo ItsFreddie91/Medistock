ca2fa3ec28da02121a715a758a72b435493f956f
Fredy <freddyjair753@gmail.com>
2025-06-13 21:12:47 -0600

    correcciones

--- src/conexion/conexion.js
+++ src/conexion/conexion.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql');
-require('dotenv').config(); // Importante para usar variables de entorno
+const mysql = require('mysql2'); // <--- usa mysql2
+require('dotenv').config();
 
 const conexion = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    port: process.env.DB_PORT, 
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
 conexion.connect((err) => {
     if (err) {
-        console.error('Error conectando a la base de datos:', err.stack);
+        console.error('❌ Error conectando a la base de datos:', err.stack);
         return;
     }
-    console.log('BASE DE DATOS CONECTADA');
+    console.log('✅ BASE DE DATOS CONECTADA');
 });
 
 module.exports = conexion;
