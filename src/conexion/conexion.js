329637c8e702531f9fd3eb0716bac7c1a42dbded
Fredy <freddyjair753@gmail.com>
2025-06-15 15:16:08 -0600

    conexion 3

--- src/conexion/conexion.js
+++ src/conexion/conexion.js
@@ -1,11 +1,11 @@
-// src/conexion/conexion.js
 const mysql = require('mysql2');
 
 const db_config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT),
 };
 
 let conexion;
@@ -16,15 +16,15 @@ function handleDisconnect() {
     conexion.connect((err) => {
         if (err) {
             console.error('Error al conectar con la base de datos:', err);
-            setTimeout(handleDisconnect, 2000); // Reintenta en 2s
+            setTimeout(handleDisconnect, 2000); // Reintenta después de 2 segundos
         } else {
-            console.log('Conectado a la base de datos');
+            console.log('Conectado a la base de datos MySQL 🚀');
         }
     });
 
-    conexion.on('error', function (err) {
+    conexion.on('error', (err) => {
+        console.error('Error de conexión:', err);
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.warn('Conexión perdida. Reintentando...');
             handleDisconnect();
         } else {
             throw err;
