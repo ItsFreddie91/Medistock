09e43c2a00f47d1140299a954330f9e10b34f084
BrandonFH205 <brandonricardofloreshuertas@gmail.com>
2025-11-20 21:21:55 -0600

    conexion

--- src/conexion/conexion.js
+++ src/conexion/conexion.js
@@ -1,57 +1,57 @@
-// const mysql = require('mysql2');
-
-// const db_config = {
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: parseInt(process.env.DB_PORT),
-// };
-
-// let conexion;
-
-// function handleDisconnect() {
-//     conexion = mysql.createConnection(db_config);
-
-//     conexion.connect((err) => {
-//         if (err) {
-//             console.error('Error al conectar con la base de datos:', err);
-//             setTimeout(handleDisconnect, 2000); // Reintenta después de 2 segundos
-//         } else {
-//             console.log('Conectado a la base de datos MySQL 🚀');
-//         }
-//     });
-
-//     conexion.on('error', (err) => {
-//         console.error('Error de conexión:', err);
-//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//             handleDisconnect();
-//         } else {
-//             throw err;
-//         }
-//     });
-// }
-
-// handleDisconnect();
-
-// module.exports = conexion;
-
-
-
 const mysql = require('mysql2');
-const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'centro'
-});
 
-conexion.connect((err) => {
-    if (err) {
-        console.error('Error conectando a la base de datos:', err.stack);
-        return;
-    }
-    console.log('BASE DE DATOS CONECTADA');
-});
+const db_config = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT),
+};
+
+let conexion;
+
+function handleDisconnect() {
+    conexion = mysql.createConnection(db_config);
+
+    conexion.connect((err) => {
+        if (err) {
+            console.error('Error al conectar con la base de datos:', err);
+            setTimeout(handleDisconnect, 2000); // Reintenta después de 2 segundos
+        } else {
+            console.log('Conectado a la base de datos MySQL 🚀');
+        }
+    });
+
+    conexion.on('error', (err) => {
+        console.error('Error de conexión:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = conexion;
+
+
+
+// const mysql = require('mysql2');
+// const conexion = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'centro'
+// });
+
+// conexion.connect((err) => {
+//     if (err) {
+//         console.error('Error conectando a la base de datos:', err.stack);
+//         return;
+//     }
+//     console.log('BASE DE DATOS CONECTADA');
+// });
+
+// module.exports = conexion;
